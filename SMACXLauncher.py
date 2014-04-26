97bc7e7c2df2b893e46e7d8cebd313e47d3605f5
Gregory <grjordan@gmail.com>
2014-04-26 13:46:12 -0500

    Can enable and disable mods.

--- SMACXLauncher.py
+++ SMACXLauncher.py
@@ -12,19 +12,16 @@ from os import path
 import configparser
 import os
 import shutil
+import time
 
-
-"""
-filename = filedialog.askopenfilename()
-subprocess.Popen(r'c:\mytool\tool.exe', cwd=r'd:\test\local')
-
-"""
 
 TERRAN_KEY = "terran_Key"
 TERRANX_KEY = "terranX_Key"
 WF_KEY = "working_folder"
 CONFIG = 'settings.ini'
 SET = "SETTINGS"
+EXCLUDED_FILES = [".tmp", ".dll", ".sys",".Ini"]
+EXCLUDED_FOLDERS = ["saves", "Color Blind Palette"]
 
 logging.basicConfig(level=logging.DEBUG)
 class app():
@@ -34,8 +31,7 @@ class app():
 		self.config[SET]={}
 
 		self.config.read(CONFIG)
-		
-	
+
 		
 		
 		self.root = tk.Tk()
@@ -63,29 +59,30 @@ class app():
 		
 		label_in_mods = ttk.Label(frameMods, text='Inactive mods:')
 		label_in_mods.grid(column=0, row=0)
-		list_available_mods = tk.Listbox(frameMods, height=20)
-		list_available_mods.grid(column=0, row=1, sticky="N,W,E,S")
-		scroll_in_mods = ttk.Scrollbar(frameMods, orient="vertical", command=list_available_mods.yview)
+		str_var_inactive_mods = tk.StringVar(value=tuple(self.get_list_of_mods())) #TODO: Have this load from memory
+		self.tklist_available_mods = tk.Listbox(frameMods, height=20, listvariable=str_var_inactive_mods)
+		self.tklist_available_mods.grid(column=0, row=1, sticky="N,W,E,S")
+		scroll_in_mods = ttk.Scrollbar(frameMods, orient="vertical", command=self.tklist_available_mods.yview)
 		scroll_in_mods.grid(column=1, row=1, sticky="N,S")
-		list_available_mods['yscrollcommand'] = scroll_in_mods.set
-		for i in range(1,101):
-			list_available_mods.insert('end', 'Line %d of 100' % i)
+		self.tklist_available_mods['yscrollcommand'] = scroll_in_mods.set
+		
+		
 			
 		label_ac_mods = ttk.Label(frameMods, text='Active mods:')
 		label_ac_mods.grid(column=4, row=0)
-		list_active_mods = tk.Listbox(frameMods, height=20)
-		list_active_mods.grid(column=4, row=1, sticky="N,W,E,S")
-		scroll_ac_mods = ttk.Scrollbar(frameMods, orient="vertical", command=list_active_mods.yview)
+		self.str_var_active_mods = tk.StringVar(value=tuple([])) #TODO: Have this load from memory
+		self.tklist_active_mods = tk.Listbox(frameMods, height=20, listvariable=self.str_var_active_mods)
+		self.tklist_active_mods.grid(column=4, row=1, sticky="N,W,E,S")
+		scroll_ac_mods = ttk.Scrollbar(frameMods, orient="vertical", command=self.tklist_active_mods.yview)
 		scroll_ac_mods.grid(column=5, row=1, sticky="N,S")
-		list_active_mods['yscrollcommand'] = scroll_ac_mods.set
-		for i in range(1,101):
-			list_active_mods.insert('end', 'Line %d of 100' % i)
+		self.tklist_active_mods['yscrollcommand'] = scroll_ac_mods.set
+		
 		
 		frame_leftrightbuttons = ttk.Frame(frameMods)
 		frame_leftrightbuttons.rowconfigure(3,minsize=20)
 		frame_leftrightbuttons.grid(column=3, row=1, padx=10)
-		button_left = ttk.Button(frame_leftrightbuttons, text='<', command=lambda: print("Left!"))
-		button_right = ttk.Button(frame_leftrightbuttons, text='>', command=lambda: print("Right!"))
+		button_left = ttk.Button(frame_leftrightbuttons, text='<', command=lambda: self.move_rightleft(self.tklist_active_mods,self.tklist_available_mods))
+		button_right = ttk.Button(frame_leftrightbuttons, text='>', command=lambda: self.move_rightleft(self.tklist_available_mods,self.tklist_active_mods))
 		button_up = ttk.Button(frame_leftrightbuttons, text='/\\', command=lambda: print("Up!"))
 		button_down = ttk.Button(frame_leftrightbuttons, text='\\/', command=lambda: print("Down!"))
 		button_left.grid(column=0, row=1)
@@ -126,11 +123,14 @@ class app():
 		# Launch Game area
 		frameLaunchers = ttk.Frame(self.root)
 		label_launch = ttk.Label(frameLaunchers, text='Start:')
-		label_launch.grid(column=0, row=0,pady=5)
+		
+		button_apply = ttk.Button(frameLaunchers, text='Force update', command=lambda: self.apply_mods(True))
 		button_SMAC = ttk.Button(frameLaunchers, text='Alpha Centauri', command=lambda: self.start_game(self.str_terran.get()))
 		button_SMACX = ttk.Button(frameLaunchers, text='Alien Crossfire', command=lambda: self.start_game(self.str_terranx.get()))
-		button_SMAC.grid(column=1, row=0,padx=10)
-		button_SMACX.grid(column=2, row=0)
+		button_apply.grid(column=0, row=0)
+		label_launch.grid(column=1, row=0,pady=5)
+		button_SMAC.grid(column=2, row=0,padx=10)
+		button_SMACX.grid(column=3, row=0)
 		frameLaunchers.grid(column=0, row=1, sticky="S, E")
 		
 		# Start the window
@@ -153,12 +153,22 @@ class app():
 				self.make_backup_folder()
 				logging.info("Finished")
 	
+	def move_rightleft(self, list_from, list_to):
+		activate = list_from.curselection()
+		for modnum in activate:
+			modnamelist = list_from.get(modnum)
+			list_to.insert('end',modnamelist)
+			list_from.selection_clear(modnum)
+			list_from.delete(modnum)
+		
 	def set_terran(self, key, string):
 		value = filedialog.askopenfilename()
 		self.config[SET][key]=value
 		string.set(value)	
 		self.save_settings()
 		
+	def get_list_of_mods(self):
+		return os.listdir("./mods/")
 	
 	def save_settings(self):
 		logging.info("Saving settings")
@@ -181,30 +191,75 @@ class app():
 		
 		# Ignore .tmp, .dll /saves /Color Blind Palette for backup folder.
 		# Autoconvert color blind into a mod.
-		fileexcludes = [".tmp", ".dll", ".sys"]
-		folders_skip = ["saves", "Color Blind Palette"]
+
 		
 		logging.info("Copy to backup folder")
 		for dirname, dirnames, files in os.walk(self.config[SET][WF_KEY]):
 			#ok remove folders we don't go into
-			for rem_f in folders_skip:
+			for rem_f in EXCLUDED_FOLDERS:
 				if rem_f in dirnames:
 					dirnames.remove(rem_f)
 			
 			for file in files:
-				if path.splitext(file)[1] not in fileexcludes:
+				if path.splitext(file)[1] not in EXCLUDED_FILES:
 					file_path = path.join(dirname, file)
 					logging.debug(file_path)
 					destination = path.join("./backup", path.relpath(path.join(dirname, file), self.config[SET][WF_KEY]))
 					self.create_folder(destination)
-					shutil.copyfile(file_path, destination)
+					shutil.copy2(file_path, destination)
 					
 
+	def apply_mods(self, force):
 		
+		dict_files_to_copy = {} # File relative path : File to copy path
+		dict_files_to_copy = self.get_file_dict("./backup", dict_files_to_copy)
+		# Get list of mods to apply
+		enabledmods = []
+		for i in range(0, self.tklist_active_mods.size()):
+			enabledmods.append(self.tklist_active_mods.get(i))
+		#enabledmods = self.str_var_active_mods.get().translate(test).split(",") #WARNING: WIll leave an empty set in list.
+		logging.debug(enabledmods)
+		for ap_mods in enabledmods:
+			if ap_mods != '':
+				dict_files_to_copy = self.get_file_dict(path.join("./mods", ap_mods), dict_files_to_copy)
 		
+	
+		# Then copy files back
+
+		logging.info("Start Copying")
+		start_time = time.time()
 		
+
+		for key, item in dict_files_to_copy.items():
+			destination = path.join(self.config[SET][WF_KEY], key)
+			
+			
+			if force or os.stat(item).st_mtime != os.stat(destination).st_mtime: # not perfect solution, but should be pretty good.
+				logging.info(item + " to " + destination)
+				shutil.copy2(item, destination)
+		elapsed_time = time.time() - start_time
+		logging.info("End copying. Time took " +str(elapsed_time))
+	
+	def get_file_dict(self, directory, dict_files_to_copy):
+		for dirname, dirnames, files in os.walk(directory):
+			#ok remove folders we don't go into
+			for rem_f in EXCLUDED_FOLDERS:
+				if rem_f in dirnames:
+					dirnames.remove(rem_f)
+			
+			for file in files:
+				if path.splitext(file)[1] not in EXCLUDED_FILES:
+					file_path = path.join(dirname, file)
+					relative_path = path.relpath(path.join(dirname, file), directory)
+					dict_files_to_copy[relative_path] = file_path
+					
+					logging.debug(relative_path + " : " + file_path)
+					
+		
+		return dict_files_to_copy	
 			
 	def start_game(self, gamelocation):
+		self.apply_mods(False)
 		print("start the game")
 		self.save_settings()	
 		# Check to see if folderlocation is valid.
